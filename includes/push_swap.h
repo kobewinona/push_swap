a9afd26c84e3d4f008e1a04fe25f50439eb4275f
Dima Klimkin <dklimkin@z1t2c4.42bangkok.com>
2023-11-22 17:00:56 +0700

    style: reorganize header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -41,9 +41,6 @@ typedef struct s_chunk
 // - src
 t_list	*parse_argv(int argc, char **argv);
 int		is_argv_valid(t_list *args);
-int		handle_error(t_stack **stack_a, t_stack **stack_b, t_list **args_lst);
-void	fill_stack(t_stack **stack, t_list *args_lst, size_t stack_size);
-int		is_stack_sorted(t_stack *stack_a);
 
 void	do_sa(t_stack **stack_a);
 void	do_sb(t_stack **stack_b);
@@ -60,25 +57,29 @@ void	do_rrr(t_stack **stack_a, t_stack **stack_b);
 void	do_pa(t_stack **stack_a, t_stack **stack_b);
 void	do_pb(t_stack **stack_a, t_stack **stack_b);
 
+void	fill_stack(t_stack **stack, t_list *args_lst, size_t stack_size);
 void	sort_stack(t_stack **a, t_stack **b, int stack_size);
 void	sort_more_than_five(t_stack **a, t_stack **b, int stack_size);
+int		count_moves_to_chunk(t_stack *stack, t_chunk *chunk, int stack_size);
+int		count_moves_to_index(t_stack *stack, int target_index);
+void	move_stack_a(int moves, t_stack **a, int stack_size);
+void	move_stack_b(int moves, t_stack **b, int stack_size);
 
 // - utils
 void	print_err_msg(void);
 void	print_stack(t_stack *stack, char *stack_name, int index);
 
+int		handle_error(t_stack **stack_a, t_stack **stack_b, t_list **args_lst);
 void	free_double_arr(char **arr);
 
+t_chunk	*init_chunk(t_stack **stack, size_t stack_size);
 t_stack	*init_stack(size_t size);
 void	stackadd_front(t_stack **stack, t_stack *node);
-int		calculate_stack_size(t_stack *stack);
 void	free_stack(t_stack **stack);
+
+int		calculate_stack_size(t_stack *stack);
+int		is_stack_sorted(t_stack *stack_a);
 ssize_t	find_stack_min(t_stack *stack);
 ssize_t	find_stack_max(t_stack *stack);
-int		count_moves_to_chunk(t_stack *stack, t_chunk *chunk, int stack_size);
-int		count_moves_to_index(t_stack *stack, int target_index);
-void	move_stack_a(int moves, t_stack **a, int stack_size);
-void	move_stack_b(int moves, t_stack **b, int stack_size);
-t_chunk	*init_chunk(t_stack **stack, size_t stack_size);
 
 #endif
